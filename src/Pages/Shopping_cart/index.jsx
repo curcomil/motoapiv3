f5a7270750ba610000dfcb1e2677ac4b2d1d3bb9
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-20 21:42:59 -0600

    Update index.jsx

--- src/Pages/Shopping_cart/index.jsx
+++ src/Pages/Shopping_cart/index.jsx
@@ -102,7 +102,7 @@ const ShoppingCart = () => {
     try {
       for (const item of cartItems) {
         await axios.put(
-          `http://localhost:3000/api/products/${item.id}/reduce-stock`,
+          `https://motoapibackv3.vercel.app/api/products/${item.id}/reduce-stock`,
           {
             quantity: item.quantity,
           }
@@ -151,7 +151,7 @@ const ShoppingCart = () => {
       <br />
       <div className="flex-grow bg-gradient-to-t from-black via-[#0faf09] p-4 sm:p-12 flex flex-col items-center">
         <h1 className="text-center text-3xl text-[#0eff06] mb-8">
-          Carrito de compras
+          Carrito de compras|
         </h1>
         <div className="bg-[#00000060] rounded-xl p-4 sm:p-12 w-full sm:w-11/12">
           <div className="bg-black rounded-xl p-4 sm:p-12">
