a3da67260cd76708c590e30c6069f66eb700da5c
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-21 15:15:33 -0600

    Update Checkoutforms.jsx

--- src/Components/Checkoutforms.jsx
+++ src/Components/Checkoutforms.jsx
@@ -9,22 +9,62 @@ import axios from "axios";
 const CheckoutForm = ({ items }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Crear PaymentIntent cuando el componente se monta
+    const createPaymentIntent = async () => {
+      try {
+        const response = await axios.post(
+          "https://motoapibackv3.vercel.app/api/create-payment-intent",
+          { items },
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = response.data;
+        if (data.clientSecret) {
+          setClientSecret(data.clientSecret);
+        } else {
+          throw new Error("No se recibió clientSecret del servidor.");
+        }
+      } catch (error) {
+        console.error("Error al crear el PaymentIntent:", error);
+      }
+    };
+
+    createPaymentIntent();
+  }, [items]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!stripe || !elements) {
+    if (!stripe || !elements || !clientSecret) {
+      console.error("Stripe, Elements, o clientSecret no están disponibles.");
       return;
     }
 
-    const { error, paymentIntent } = await stripe.confirmPayment({
+    // Primero, somete los elementos del formulario
+    const { error: submitError } = await elements.submit();
+    if (submitError) {
+      console.error("Error al someter los elementos:", submitError.message);
+      return;
+    }
+
+    // Luego, confirma el pago
+    const { error: confirmError } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "https://your-return-url.com",
+        return_url: "https://motoapiv3.vercel.app/Shopping",
       },
+      clientSecret, // Asegúrate de pasar el clientSecret aquí
     });
 
-    if (!error) {
+    if (!confirmError) {
       try {
         // Reducción del stock después del pago exitoso
         await Promise.all(
@@ -37,12 +77,11 @@ const CheckoutForm = ({ items }) => {
             );
           })
         );
-        // Mostrar mensaje de éxito, redirigir, etc.
       } catch (error) {
         console.error("Error al reducir el stock:", error);
       }
     } else {
-      console.error("Error en el proceso de pago:", error.message);
+      console.error("Error en el proceso de pago:", confirmError.message);
     }
   };
 
@@ -51,7 +90,7 @@ const CheckoutForm = ({ items }) => {
       <PaymentElement />
       <button
         type="submit"
-        disabled={!stripe}
+        disabled={!stripe || !clientSecret}
         className="p-2 bg-[#0EFF06] rounded-lg w-full text-black mt-6"
       >
         Pagar
